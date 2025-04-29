bf5e84d8e2ed23a4e0aed5434aa8123fd8524be3
DouglasGomes0 <douglasgomesaqui@gmail.com>
2025-04-29 15:18:08 -0300

    inicio do projeto
    desenvolvimento da interface e códigos da calculadora

---
+++ CalculadoraSimples/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace CalculadoraSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.rbSoma = new System.Windows.Forms.RadioButton();
+            this.rbSub = new System.Windows.Forms.RadioButton();
+            this.rbDiv = new System.Windows.Forms.RadioButton();
+            this.rbMult = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(143, 59);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(144, 100);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero1.Location = new System.Drawing.Point(52, 62);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(60, 13);
+            this.lblNumero1.TabIndex = 2;
+            this.lblNumero1.Text = "Numero 1";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero2.Location = new System.Drawing.Point(53, 103);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(60, 13);
+            this.lblNumero2.TabIndex = 3;
+            this.lblNumero2.Text = "Numero 2";
+            // 
+            // rbSoma
+            // 
+            this.rbSoma.AutoSize = true;
+            this.rbSoma.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSoma.Location = new System.Drawing.Point(144, 169);
+            this.rbSoma.Name = "rbSoma";
+            this.rbSoma.Size = new System.Drawing.Size(54, 17);
+            this.rbSoma.TabIndex = 4;
+            this.rbSoma.TabStop = true;
+            this.rbSoma.Text = "soma";
+            this.rbSoma.UseVisualStyleBackColor = true;
+            // 
+            // rbSub
+            // 
+            this.rbSub.AutoSize = true;
+            this.rbSub.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSub.Location = new System.Drawing.Point(144, 193);
+            this.rbSub.Name = "rbSub";
+            this.rbSub.Size = new System.Drawing.Size(87, 17);
+            this.rbSub.TabIndex = 5;
+            this.rbSub.TabStop = true;
+            this.rbSub.Text = "subtração";
+            this.rbSub.UseVisualStyleBackColor = true;
+            // 
+            // rbDiv
+            // 
+            this.rbDiv.AutoSize = true;
+            this.rbDiv.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDiv.Location = new System.Drawing.Point(144, 217);
+            this.rbDiv.Name = "rbDiv";
+            this.rbDiv.Size = new System.Drawing.Size(67, 17);
+            this.rbDiv.TabIndex = 6;
+            this.rbDiv.TabStop = true;
+            this.rbDiv.Text = "divisão";
+            this.rbDiv.UseVisualStyleBackColor = true;
+            // 
+            // rbMult
+            // 
+            this.rbMult.AutoSize = true;
+            this.rbMult.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMult.Location = new System.Drawing.Point(144, 241);
+            this.rbMult.Name = "rbMult";
+            this.rbMult.Size = new System.Drawing.Size(108, 17);
+            this.rbMult.TabIndex = 7;
+            this.rbMult.TabStop = true;
+            this.rbMult.Text = "multiplicação";
+            this.rbMult.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(95, 289);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(200, 50);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Font = new System.Drawing.Font("Goudy Stout", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(174, 354);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(0, 37);
+            this.lbResultado.TabIndex = 11;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(151, 413);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(73, 31);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(347, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rbMult);
+            this.Controls.Add(this.rbDiv);
+            this.Controls.Add(this.rbSub);
+            this.Controls.Add(this.rbSoma);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.RadioButton rbSoma;
+        private System.Windows.Forms.RadioButton rbSub;
+        private System.Windows.Forms.RadioButton rbDiv;
+        private System.Windows.Forms.RadioButton rbMult;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ CalculadoraSimples/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        { //pegar numeros da interface
+            int n1, n2;
+           n1 = int.Parse(txtNum1.Text);
+           n2 = int.Parse(txtNum2.Text);
+
+            //verificar a opc marcada
+            if (rbSoma.Checked)
+            {
+                lbResultado.Text = (n1 + n2).ToString();
+            }
+            else if (rbSub.Checked)
+            {
+                lbResultado.Text = (n1 - n2).ToString();
+            }
+            else if (rbMult.Checked)
+            {
+                lbResultado.Text = (n1 * n2).ToString();
+            }
+            else if (rbDiv.Checked)
+            {
+                if (n2 != 0)
+                {
+                    lbResultado.Text = (n1/n2).ToString();
+                }
+                else
+                {
+                    lbResultado.Text = "ERRO";
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //retorna os nud's ao svalores originais:
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            lbResultado.Text = "";
+        }
+    }
+}
